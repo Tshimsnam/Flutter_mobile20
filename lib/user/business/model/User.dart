27a0a8b992e7988ead921c100e3a1934ad6bc39c
Tshimsnam <manassetshims@gmail.com>
2024-05-03 14:37:17 +0100

    envoie code

---
+++ lib/user/business/model/User.dart
@@ -0,0 +1,33 @@
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  int id;
+  String name;
+  String email;
+
+  User({
+    this.id=-1,
+    this.name="",
+    this.email="",
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json["id"],
+    name: json["name"],
+    email: json["email"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "email": email,
+  };
+}
